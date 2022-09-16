19375ae038ceede90c6da57c3749477d84b0333a
Patrick <82187582+patshash@users.noreply.github.com>
2022-09-16 16:28:04 +1000

    add new line

--- main.tf
+++ main.tf
@@ -66,6 +66,7 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  // new line comment. 
   egress {
     from_port   = 0
     to_port     = 0
